5e8f6cceb74f29d51f36929c1c2841978bda83b3
Damianpon <damian.cules.96@gmail.com>
2020-05-09 09:38:07 +0200

    Odp 3.

--- zjazd 3/Sprwadzian/spr_01.py
+++ zjazd 3/Sprwadzian/spr_01.py
@@ -1,16 +1,13 @@
-##1. Wymień poznane dotąd struktury danych.
+##3. Zaimplementuj funkcję realizującą algorytm sortowanie przez wybieranie
+def wymieranie_przez_sortowanie(lista):
+    for i in range(len(lista)):
+        wart_min = i
+        for j in range(1 + i, len(lista)):
+            if lista[j] < lista[wart_min]:
+                wart_min = j
+        lista[i], lista[wart_min] = lista[wart_min], lista[i]
 
-#print("""Niemutowalne:\n
-#bool,
-#string,
-#tuple,
-#int,
-#float.
-#""")
+    return lista
 
-#print("""Mutowalne typy:\n
-#list,
-#dict.
-#""")
-
+print(wymieranie_przez_sortowanie([9, 1, 6, 8, 4, 3, 2, 0]))
 
