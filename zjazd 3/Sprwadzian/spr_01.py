6a9816e217d859e3575f0e3951fd94048a6c7cc3
Damianpon <damian.cules.96@gmail.com>
2020-05-09 09:55:36 +0200

    Odp 5..

--- zjazd 3/Sprwadzian/spr_01.py
+++ zjazd 3/Sprwadzian/spr_01.py
@@ -1,7 +1,21 @@
-##3. 4. Masz słownik
-zrodla = {"a": 10, "b":30}
+##5. Zdefiniuj funkcję foo, która przyjmie dowolną liczbę argumentów pozycyjnych i kluczowych
+##   funkcja ma wypisać na ile ich jest
 
-#Jak bezpiecznie wybrać z tego słównika wartość i przypisać ją do zmiennej, to by nie był rzucony błąd?
-#Np. chcesz sprawdzić dla wartości "c", której nie ma w słowniku
 
-print(zrodla.get("c",))
+#foo(10, 20, a=1, b=2, c=3)
+#   pozycyjnych: 2
+#   kluczowych:  3
+
+def foo(*args,**kwargs):
+    liczba_args = []
+    liczba_kwargs = []
+    for i in args:
+        liczba_args.append(i)
+    for j in kwargs:
+        liczba_kwargs.append((j))
+
+    print(f' pozycyjnych: {len(liczba_args)}')
+    print(f' kluczowych: {len(liczba_kwargs)}')
+
+    return ""
+print((foo(10, 20, a=1, b=2, c=3)))
