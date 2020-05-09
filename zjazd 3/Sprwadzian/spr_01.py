8130eaf054b8b7ac85b07305861e98e231fc0f71
Damianpon <damian.cules.96@gmail.com>
2020-05-09 09:31:10 +0200

    Odp 2.

--- zjazd 3/Sprwadzian/spr_01.py
+++ zjazd 3/Sprwadzian/spr_01.py
@@ -1,15 +1,16 @@
 ##1. Wymień poznane dotąd struktury danych.
 
-print("""Niemutowalne:\n
-bool,
-string,
-tuple,
-int,
-float.
-""")
+#print("""Niemutowalne:\n
+#bool,
+#string,
+#tuple,
+#int,
+#float.
+#""")
 
-print("""Mutowalne typy:\n
-list,
-dict.
-""")
+#print("""Mutowalne typy:\n
+#list,
+#dict.
+#""")
+
 
